d9ba530dc386d6487ba8a27909335ce43648c0ca
15915940450 <15915940450@139.com>
2017-09-12 14:05:17 +0800

    component on and emit ev

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const Moon=require('moonjs');
 Moon.component('counter-component',{
   data:function(){
     return {
-      count:15816277995
+      count:9
     };
   },
   methods:{
     increment:function(){
       this.set('count',this.get('count')+1);
+      this.emit('it');  //m-on:it="incrementTotal"
     }
   },
   template:`<div>
@@ -17,6 +18,16 @@ Moon.component('counter-component',{
   </div>`
 });
 
+//container---counter-component
+
 new Moon({
-  el:'#container'
+  el:'#container',
+  data:{
+    total:18
+  },
+  methods:{
+    incrementTotal:function(){
+      this.set('total',this.get('total')+1);
+    }
+  }
 });
