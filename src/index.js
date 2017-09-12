837a520a832db5f2dbfbeb74c9da9b4d102233fa
15915940450 <15915940450@139.com>
2017-09-12 14:52:55 +0800

    use slot nor props

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 const Moon=require('moonjs');
 
-Moon.component('counter-component',{
-  data:function(){
-    return {
-      count:9
-    };
-  },
-  methods:{
-    increment:function(){
-      this.set('count',this.get('count')+1);
-      this.emit('it');  //m-on:it="incrementTotal"
-    }
-  },
+Moon.component('slot-component',{
   template:`<div>
-    <p>Count: {{count}}</p>
-    <button m-on:click="increment">Increment</button>
+    <slot name="container__h1"></slot>
+    <slot name="container__p"></slot>
+    <slot name="container__h6"></slot>
   </div>`
 });
-
-//container---counter-component
 
 new Moon({
   el:'#container',
   data:{
-    total:18
-  },
-  methods:{
-    incrementTotal:function(){
-      this.set('total',this.get('total')+1);
-    }
+    myname:'Thilina fang',
+    time:'2017-09-12T22:22'
   }
 });
