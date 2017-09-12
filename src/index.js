931d7c462841bd3419f8c02fbcef0a1eebff482c
15915940450 <15915940450@139.com>
2017-09-12 10:00:20 +0800

    methods

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const Moon=require('moonjs');
-
+// console.log(Moon.version);  //0.11.0
 new Moon({
   el:'#container',
   data:{
     phoneNumber:15816277995
+  },
+  methods:{
+    increment:function(){
+      this.set('phoneNumber',this.get('phoneNumber')+1);
+    }
   }
 });
