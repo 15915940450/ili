cf3f700042c74a01a08e7e5dd1c662b739531e21
15915940450 <15915940450@139.com>
2017-09-13 15:44:11 +0800

    m-for in

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 new Moon({
-  el: '#container',
-  data: {
-    list: ['Item - 1', 'Item - 2', 'Item - 3', 'Item - 4']
+  el:'#container',
+  data:{
+    objAllEBike:[{
+      sn:100121,
+      mac:'09-54-11-2d-c5-22',
+      date:'2017-9-30',
+      area:'宝安',
+      state:'已入库'
+    },{
+      sn:100126,
+      mac:'09-54-11-2d-c5-26',
+      date:'2017-9-26',
+      area:'南山',
+      state:'已出库'
+    },{
+      sn:100129,
+      mac:'09-54-11-2d-c5-29',
+      date:'2017-9-29',
+      area:'龙岗',
+      state:'有故障'
+    }]
   }
 });
