4465070be38872bf863a792ef272ddd26dbe73e2
15915940450 <15915940450@139.com>
2017-09-20 10:40:58 +0800

    foo table

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
-global.Moon=require('moonjs');
-const MoonRouter=require('moon-router');
-Moon.use(MoonRouter);
+// var $=require('jquery');
 
-const router=new MoonRouter({
-  default:'/',
-  map:{
-    '/':'root',
-    '/hello':'hello'
+// console.log($('.fooTable').jquery);
+// console.log($());
+
+$('.fooTable').footable({
+  filtering:{
+    enabled:true,
+    container:'#filter'
   }
-});
-
-
-
-
-Moon.component('root', {
-  template: `<div>
-    <h1>Welcome to "/"</h1>
-    <router-link to="/hello">To /hello</router-link>
-  </div>`
-});
-
-Moon.component('hello', {
-  template: `<div>
-    <h1>You have Reached "/hello"</h1>
-    <router-link to="/">Back Home</router-link>
-  </div>`
-});
-
-
-new Moon({
-  el:'#container',
-  router:router
 });
