beeb2fd92807d2baa7b5937239a76562933adde2
15915940450 <15915940450@139.com>
2017-09-28 17:46:04 +0800

    thilina-at-immotor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-const $=require('jquery');
 const Mock=require('mockjs');
+const $=require('jquery');
 
-Mock.mock('http://mockjs',{
-  'list|1-5':[{
-    'id|+1':1,
-    'hhj':'@name()',
-    'age|1-100':100
+Mock.mock('http://thilina-at-immotor',{
+  'jsonBatteryStation|0-9':[{
+    'stationID':'@string("upper", 2)@id()',
+    'hasBattery|0-10':10,
+    'location':'深圳@county()@cword(2,5)大道@natural(2,200)号',
+    'll':'@float(113, 114, 6, 6),@float(22, 22, 6, 6)',
+    'firmwareVersion':'V @natural(0, 10).@natural(0, 10).@natural(0, 10)',
+    'hardwareVersion':'V @natural(0, 10).@natural(0, 10).@natural(0, 10)'
   }]
 });
 
-$.get('http://mockjs').done(function(data){
-  console.log(data);
+$.ajax({
+  url:'http://thilina-at-immotor',
+  method:'GET',
+  dataType:'json',
+  error:function(err){
+    console.log(err);
+  }
+}).done(function(jsonData){
+  console.log(jsonData);
 });
